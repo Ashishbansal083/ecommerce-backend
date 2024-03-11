3660f43a2893d7752e654a472dfe330963bd4f02
Ashish <bansalgashi083@gmail.com>
2024-03-11 14:54:41 +0530

    fetch all products added

--- controller/Product.js
+++ controller/Product.js
@@ -9,3 +9,26 @@ exports.createProduct = async (req, res) => {
     res.status(400).json(err);
   }
 };
+exports.fetchAllProducts = async (req, res) => {
+  const query = Product.find({});
+  if(req.query.category){
+    query =  query.find({category:req.query.category});
+  }
+  if(req.query.brand){
+    query =  query.sort({brand:req.query.brand});
+  }
+  if(req.query._sort && req.query._order){
+    query =  query.sort({[req.query._sort]:req.query._order});
+  }
+  if(req.query._page && req.query._per_page){ 
+    const pageSize = req.query._per_page;
+    const page = req.query._page;
+    query =  query.skip(pageSize*(page-1)).limit(pageSize);
+  } 
+  try{
+    const docs = await query.exec();
+    res.status(200).json(docs);
+  }catch(err){
+    res.status(400).json(err);
+  }
+};
