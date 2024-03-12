3fb329fe2fd36eba9e621a5ec406645fe01197e9
Ashish <bansalgashi083@gmail.com>
2024-03-12 14:04:38 +0530

    minor changes

--- controller/Product.js
+++ controller/Product.js
@@ -11,15 +11,25 @@ exports.createProduct = async (req, res) => {
 };
 exports.fetchAllProducts = async (req, res) => {
   let query = Product.find({});
+  let totalProductsQuery = Product.find({});
+
   if(req.query.category){
     query =  query.find({category:req.query.category});
+    totalProductsQuery = totalProductsQuery.find({category:req.query.category});
+
   }
   if(req.query.brand){
     query =  query.sort({brand:req.query.brand});
+    totalProductsQuery = totalProductsQuery.find({brand:req.query.brand});
+
   }
   if(req.query._sort && req.query._order){
-    query =  query.sort({[req.query._sort]:req.query._order});
+    query =  query.sort({[req.query._sort]:req.query._order});   
+
   }
+
+  const totalDocs = await totalProductsQuery.count().exec();
+  console.log(totalDocs)
   if(req.query._page && req.query._per_page){ 
     const pageSize = req.query._per_page;
     const page = req.query._page;
@@ -27,6 +37,7 @@ exports.fetchAllProducts = async (req, res) => {
   } 
   try{
     const docs = await query.exec();
+    res.set('X-Total-Count',totalDocs)
     res.status(200).json(docs);
   }catch(err){
     res.status(400).json(err);
