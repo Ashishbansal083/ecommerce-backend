1fc2d57de109ac621cfb5d5cd09a291b4180bb31
Ashish <bansalgashi083@gmail.com>
2024-03-13 15:10:30 +0530

    fetchproduct by id added

--- routes/Products.js
+++ routes/Products.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { createProduct, fetchAllProducts } = require('../controller/Product');
+const { createProduct, fetchAllProducts, fetchProductById } = require('../controller/Product');
 
 const router = express.Router();
 router.post('/',createProduct)
-      .get('/',fetchAllProducts);
+      .get('/',fetchAllProducts)
+      .get('/:id',fetchProductById);
 exports.router = router;
