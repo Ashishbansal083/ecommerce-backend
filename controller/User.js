0ff22930e58b445731f97df172778f558165912f
Ashish <bansalgashi083@gmail.com>
2024-03-26 16:22:33 +0530

    order issue fixed

--- controller/User.js
+++ controller/User.js
@@ -4,7 +4,7 @@ exports.fetchUserById = async (req, res) => {
   const {id} = req.params;
   
   try{
-    const user = await User.findById(id,'name email id')    
+    const user = await User.findById(id,'name email id addresses')    
     res.status(200).json(user);
   }catch(err){
     res.status(400).json(err);
