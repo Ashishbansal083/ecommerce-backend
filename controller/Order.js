185ba9d97016eea09d3f06294304c588361126fe
Ashish <bansalgashi083@gmail.com>
2024-03-28 16:54:52 +0530

    minor changes

--- controller/Order.js
+++ controller/Order.js
@@ -11,11 +11,37 @@ exports.createOrder = async (req, res) => {
 };
 
 exports.fetchOrderByUser = async (req, res) => {
-  const {user} = req.query;
+  const {userId} = req.params;
   try {
-    const orders = await Order.find({ user: user })           
+    const orders = await Order.find({ user: userId})           
     res.status(200).json(orders);
   } catch (err) {
+    res.status(400).json(err);
+  }
+};
+exports.fetchAllOrders = async (req, res) => {
+  let query = Order.find({deleted:{$ne:true}});
+  let totalOrdersQuery = Order.find({deleted:{$ne:true}});
+
+ 
+  if(req.query._sort && req.query._order){
+    query =  query.sort({[req.query._sort]:req.query._order});   
+
+  }
+
+  const totalDocs = await totalOrdersQuery.count().exec();
+  
+  if(req.query._page && req.query._per_page){ 
+    const pageSize = req.query._per_page;
+    const page = req.query._page;
+    query =  query.skip(pageSize*(page-1)).limit(pageSize);
+  } 
+  try{
+    const docs = await query.exec();
+    res.set('X-Total-Count',totalDocs);
+    res.set('Access-Control-Expose-Headers', 'X-Total-Count');     
+    res.status(200).json(docs);
+  }catch(err){
     res.status(400).json(err);
   }
 };
