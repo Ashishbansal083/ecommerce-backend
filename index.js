f37047fdd269cbeb9d500a2b7a9a0c7a9c531150
Ashish <bansalgashi083@gmail.com>
2024-04-07 22:15:48 +0530

    passportjs added for authorisation

--- index.js
+++ index.js
@@ -3,26 +3,76 @@ const server = express();
 const mongoose = require("mongoose");
 const { createProduct } = require("./controller/Product");
 const productsRouter = require("./routes/Products");
-const brandsRouter = require('./routes/Brands')
-const categoriesRouter = require('./routes/Categories')
-const authRouter = require('./routes/Auth')
-const userRouter = require('./routes/User')
-const cartRouter = require('./routes/Cart')
-const orderRouter = require('./routes/Order')
-const cors = require("cors")
+const brandsRouter = require("./routes/Brands");
+const categoriesRouter = require("./routes/Categories");
+const authRouter = require("./routes/Auth");
+const userRouter = require("./routes/User");
+const cartRouter = require("./routes/Cart");
+const orderRouter = require("./routes/Order");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+
+server.use(
+  session({
+    secret: "keyboard cat",
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    store: new SQLiteStore({ db: "sessions.db", dir: "./var/db" }),
+  })
+);
+server.use(passport.authenticate("session"));
 
 //middlewares
-server.use(cors())
+server.use(cors());
 server.use(express.json()); // to parse req.body
-server.use('/products',productsRouter.router);
-server.use('/brands',brandsRouter.router);
-server.use('/categories',categoriesRouter.router);
-server.use('/users',userRouter.router);
-server.use('/auth',authRouter.router);
-server.use('/cart',cartRouter.router);
-server.use('/orders',orderRouter.router);
+server.use("/products", productsRouter.router);
+server.use("/brands", brandsRouter.router);
+server.use("/categories", categoriesRouter.router);
+server.use("/users", userRouter.router);
+server.use("/auth", authRouter.router);
+server.use("/cart", cartRouter.router);
+server.use("/orders", orderRouter.router);
 // Set the headers to be exposed
 
+//passport startagies
+
+passport.use(
+  new LocalStrategy(async function (username, password, done) {
+    try {
+      const user = await User.findOne({ email: username });
+      if (!user) {
+        done(null, false, { massage: "no such user exist" });
+      } else if (user.password === req.body.password) {
+        done({
+          id: user.id,
+          role: user.role,
+        });
+      } else {
+        done({ massage: "invailid cradantial" });
+      }
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+//this creates session vaiables
+
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+      picture: user.picture,
+    });
+  });
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
 
 main().catch((error) => console.log(error));
 
@@ -34,7 +84,7 @@ async function main() {
 server.get("/", (req, res) => {
   res.json({ status: "runnig" });
 });
-server.post("/products", createProduct); 
+server.post("/products", createProduct);
 
 server.listen(8080, () => {
   console.log("app started");
