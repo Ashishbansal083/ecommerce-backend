6684e791574e220bb477b7150f6e0a1f16827cb4
Ashish <bansalgashi083@gmail.com>
2024-04-24 17:15:16 +0530

    new build added

--- controller/User.js
+++ controller/User.js
@@ -2,8 +2,6 @@ const User = require('../model/User')
 
 exports.fetchUserById = async (req, res) => {
   const {id} = req.user;
-  console.log(id);
-  
   try{
     const user = await User.findById(id)        
     res.status(200).json({id:user.id,addresses:user.addresses,email:user.email,role:user.role});
@@ -15,7 +13,6 @@ exports.updateUser = async (req, res) => {
   const {id} = req.params;  
   try{
     const user = await User.findByIdAndUpdate(id,req.body,{new:true});
-    console.log(user)
     res.status(200).json(user);
   }catch(err){
     res.status(400).json(err);
